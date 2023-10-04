e3f13a5e6c0fd5e791a9cf73f199e582da59de93
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-10-04 09:54:03 +0100

    updated constants in home page

---
+++ renderer/renderer.js
@@ -0,0 +1,4 @@
+
+document.getElementById('appName').textContent = window.constants.app_name;
+document.getElementById('appWelcome').textContent = window.constants.app_welcome;
+document.getElementById('appVersion').textContent = window.constants.app_version;
