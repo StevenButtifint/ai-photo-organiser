8d7fab2bcc087e8058ffc161c8ae4ea91bc6edd2
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-10-05 13:09:55 +0100

    added home page button event listeners

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -2,3 +2,12 @@
 document.getElementById('appName').textContent = window.constants.app_name;
 document.getElementById('appWelcome').textContent = window.constants.app_welcome;
 document.getElementById('appVersion').textContent = window.constants.app_version;
+
+document.getElementById('getStartedButton').addEventListener('click', () => {
+  window.location.href = '../pages/setup.html';
+});
+
+document.getElementById('aboutButton').addEventListener('click', () => {
+  window.location.href = '../pages/about.html';
+});
+
