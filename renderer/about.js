7493b09fb2fb93bf0f56f1f403439942fa65ef2d
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-10-09 13:48:57 +0100

    added return home event listener

--- renderer/about.js
+++ renderer/about.js
@@ -1,3 +1,8 @@
 document.getElementById('aboutName').textContent = window.constants.app_name + " - About Us";
 document.getElementById('appName').textContent = window.constants.app_name;
 document.getElementById('appVersion').textContent = window.constants.app_version;
+
+
+document.getElementById('homeButton').addEventListener('click', () => {
+    window.location.href = '../pages/index.html';
+  });
