4605b70e3a457b95d97a22c0affe873f39aba1d5
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-10-12 18:54:04 +0100

    updated initialisation

--- backend/photo_organiser.py
+++ backend/photo_organiser.py
@@ -1,6 +1,14 @@
+import os
+import sys
+import glob
+
+from image_classifier import ImageClassifier
 
 
 class PhotoOrganiser:
     def __init__(self, directory):
         self.directory = directory
+        self.image_classifier = ImageClassifier()
+        self.image_classifier.setup_model()
+
 
