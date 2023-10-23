2c050a0763da288d2ed45951b73dd8ba2166162a
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-10-23 17:34:54 +0100

    added organise operation signal on renderer

--- renderer/setup.js
+++ renderer/setup.js
@@ -5,8 +5,16 @@ document.getElementById('setupName').textContent = window.constants.app_name + "
 document.getElementById('appName').textContent = window.constants.app_name;
 document.getElementById('appVersion').textContent = window.constants.app_version;
 
+const processButton = document.getElementById('processButton');
+
 document.getElementById('homeButton').addEventListener('click', () => {
   window.location.href = '../pages/index.html';
+});
+
+processButton.addEventListener('click', () => {
+  window.location.href = '../pages/processing.html';
+  const folder = document.getElementById("folder-entry").value;
+  ipcRenderer.send('organise-folder', folder);
 });
 
 ipcRenderer.on('organise-result', (event, result) => {
