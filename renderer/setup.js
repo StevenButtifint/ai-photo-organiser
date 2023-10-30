7355cbce9749f3dee9107a05cf87f8d8200763bd
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-10-30 14:36:45 +0000

    refined setup page

--- renderer/setup.js
+++ renderer/setup.js
@@ -2,7 +2,6 @@
 const { ipcRenderer } = require('electron');
 
 document.getElementById('setupName').textContent = window.constants.app_name + " - Setup";
-document.getElementById('appName').textContent = window.constants.app_name;
 document.getElementById('appVersion').textContent = window.constants.app_version;
 
 const processButton = document.getElementById('processButton');
