cd155343500b8e5c522c5c0b3a96cc44d3927b34
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-11-02 08:47:35 +0000

    added output status method

--- backend/photo_organiser.py
+++ backend/photo_organiser.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import glob
+import json
 
 from image_classifier import ImageClassifier
 from constants import *
@@ -10,6 +11,7 @@ class PhotoOrganiser:
     def __init__(self):
         self.status = 200
         self.directory = None
+        self.finished = False
         self.image_classifier = ImageClassifier()
         self.image_classifier.setup_model()
 
@@ -22,4 +24,10 @@ class PhotoOrganiser:
                 self.status = 404
         else:
             self.status = 400
+
+    def output_status(self, message):
+        formatted = [message, self.status, self.finished, status_codes[self.status]]
+        print(json.dumps(formatted), flush=True)
+        if self.status != 200:
+            quit()
 
