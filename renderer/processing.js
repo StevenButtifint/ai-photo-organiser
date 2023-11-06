ee8fe8d2aa4710e9bf4a3b4f973272846eca919a
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-11-06 19:34:09 +0000

    added back event listener

--- renderer/processing.js
+++ renderer/processing.js
@@ -5,3 +5,7 @@ document.getElementById('setupName').textContent = window.constants.app_name + "
 document.getElementById('appCredit').textContent = window.constants.app_credit;
 document.getElementById('appVersion').textContent = window.constants.app_version;
 
+document.getElementById('homeButton').addEventListener('click', () => {
+  window.location.href = '../pages/index.html';
+});
+ 
