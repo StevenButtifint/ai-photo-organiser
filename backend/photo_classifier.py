878c55b149c8884abebdc121d23886bf88eb9d85
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-11-09 09:29:13 +0000

    added model loaded method

--- backend/photo_classifier.py
+++ backend/photo_classifier.py
@@ -7,3 +7,6 @@ import torchvision.transforms as transforms
 class PhotoClassifier:
     def __init__(self):
         self.model = None
+    def is_model_loaded(self):
+        return self.model_loaded
+
