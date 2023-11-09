e85bc8d3cfaff7ade5453ade74c6e6ca0f4a6916
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-11-09 09:29:45 +0000

    added model setup method

--- backend/photo_classifier.py
+++ backend/photo_classifier.py
@@ -7,6 +7,22 @@ import torchvision.transforms as transforms
 class PhotoClassifier:
     def __init__(self):
         self.model = None
+        self.model_loaded = False
+
+    def setup_model(self):
+        try:
+            self.model = timm.create_model('efficientnet_b3', pretrained=True)
+            self.model.eval()
+            self.model_loaded = True
+            state = 200
+        except timm.models.model_names:
+            state = 500
+        except timm.pretrained_weights:
+            state = 501
+        except Exception as e:
+            state = 502
+        return state
+
     def is_model_loaded(self):
         return self.model_loaded
 
