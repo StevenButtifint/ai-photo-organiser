331c959d0517d1b3cc3a46658c1282d4fdf361ed
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-11-13 17:08:03 +0000

    added class index attribute and method

--- backend/photo_classifier.py
+++ backend/photo_classifier.py
@@ -1,5 +1,6 @@
 import timm
 import torch
+import json
 from PIL import Image
 import torchvision.transforms as transforms
 
@@ -7,6 +8,7 @@ import torchvision.transforms as transforms
 class PhotoClassifier:
     def __init__(self):
         self.model = None
+        self.class_index = None
         self.model_loaded = False
 
     def setup_model(self):
@@ -14,6 +16,7 @@ class PhotoClassifier:
             self.model = timm.create_model('efficientnet_b3', pretrained=True)
             self.model.eval()
             self.model_loaded = True
+            self.load_classes()
             state = 200
         except timm.models.model_names:
             state = 500
@@ -25,4 +28,8 @@ class PhotoClassifier:
 
     def is_model_loaded(self):
         return self.model_loaded
+
+    def load_classes(self):
+        with open(CLASSES_DIR, 'r') as file:
+            self.class_index = json.load(file)
 
