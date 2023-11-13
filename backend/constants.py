e2bfe5e8d843844dfacb09b3ad0fb8481b7e65c5
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-11-13 17:06:13 +0000

    added status codes

--- backend/constants.py
+++ backend/constants.py
@@ -5,4 +5,13 @@ status_codes = {
     401: "Unauthorized",
     403: "Forbidden",
     404: "Folder location was not found.",
+    500: "Photo classifier model could not be loaded.",
+    501: "Photo classifier weights could not be loaded.",
+    502: "Classifier class index could not be loaded.",
+    503: "Photo classifier could not setup correctly.",
+    504: "Photos could not be found.",
+    505: "Subfolder location could not be found.",
+    506: "Insufficient permissions for subfolder location.",
+    507: "Photo could not found and was not moved.",
+    900: "A broader exception occurred, check console message."
 }
