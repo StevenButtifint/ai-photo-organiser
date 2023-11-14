63d04194347ea862efaedfd74d3fbed667bd82b3
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-11-14 11:50:03 +0000

    added state message

--- backend/photo_classifier.py
+++ backend/photo_classifier.py
@@ -12,6 +12,7 @@ class PhotoClassifier:
         self.model_loaded = False
 
     def setup_model(self):
+        state_message = ""
         try:
             self.model = timm.create_model('efficientnet_b3', pretrained=True)
             self.model.eval()
@@ -22,9 +23,12 @@ class PhotoClassifier:
             state = 500
         except timm.pretrained_weights:
             state = 501
-        except Exception as e:
+        except FileNotFoundError:
             state = 502
-        return state
+        except Exception as e:
+            state = 900
+            state_message = str(e)
+        return state, state_message
 
     def is_model_loaded(self):
         return self.model_loaded
