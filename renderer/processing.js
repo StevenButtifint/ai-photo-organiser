120b7dabd89addd8f899e8c23c5e56dc9106d144
StevenButtifint <87138797+StevenButtifint@users.noreply.github.com>
2023-11-16 11:08:18 +0000

    added operation status update

--- renderer/processing.js
+++ renderer/processing.js
@@ -6,6 +6,7 @@ document.getElementById('appCredit').textContent = window.constants.app_credit;
 document.getElementById('appVersion').textContent = window.constants.app_version;
 
 const notice = document.getElementById('organise-notice');
+const home_button = document.getElementById('homeButton');
 
 document.getElementById('homeButton').addEventListener('click', () => {
   window.location.href = '../pages/index.html';
@@ -21,8 +22,25 @@ ipcRenderer.on('organise-result', (event, result) => {
 
   notice.textContent = `${returned_message}`;
 
+
   if (returned_state != "200"){
+    setHomeButtonText("Close");
+    home_button.className = "button";
+  }
+
+  if (returned_state == "900"){
     notice.textContent = `${returned_state_message}`;
+  } else {
+    notice.textContent = `${returned_message}`;
+  }
+
+  if (returned_done == true) {
+    setHomeButtonText("Finish");
+    home_button.className = "button";
   }
 
 });
+
+function setHomeButtonText(text) {
+  home_button.textContent = text;
+}
